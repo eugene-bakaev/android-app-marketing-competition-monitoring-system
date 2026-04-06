8cacbe25b2bdc635e1eb3be018fcd2140c9236b7
Eugene Bakaev <eugene.bakaev@gmail.com>
2026-04-06 21:05:32 +0300

    feature: add shared types, enums, and utility functions

---
+++ packages/shared/src/enums.ts
@@ -0,0 +1,12 @@
+export enum IntervalUnit {
+  MINUTE = 'MINUTE',
+  HOUR = 'HOUR',
+  DAY = 'DAY',
+  WEEK = 'WEEK',
+  MONTH = 'MONTH',
+}
+
+export enum ScreenshotStatus {
+  SUCCESS = 'SUCCESS',
+  FAILED = 'FAILED',
+}
